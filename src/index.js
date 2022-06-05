17cb3ff849a07d5369fa2f45cdc7c94a08a3993a
jangismarifet <jangismarifet@gmail.com>
2022-06-05 22:40:30 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const form = document.getElementById("create-task-form");
+const tasks = document.getElementById("tasks");
+
+
+const submit = (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  let todo = form[0].value;
+  let li = document.createElement('li');
+  li.textContent = todo;
+  tasks.appendChild(li);
+
+}
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", submit);
 });
